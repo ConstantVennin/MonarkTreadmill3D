bd817730d6cb4e8b180fb7c29868a670a0ad9aba
ValouZz59 <valentin.giroire59@gmail.com>
2022-05-30 17:08:58 +0200

    TITI

--- Main.gd
+++ Main.gd
@@ -1,24 +1,19 @@
 extends Node
 
 export(PackedScene) var mob_scene
-
+var speed2=10
 
 func _ready():
 	randomize()
 	$UserInterface/Retry.hide()
-	#$Stream.play()
-
-
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
-		get_tree().reload_current_scene()
-		$Stream.play()
-
+	
 
 func _on_MobTimer_timeout():
 	# Create a Mob instance and add it to the scene.
 	var mob = mob_scene.instance()
 	var direction = Vector3.ZERO
+
+
 
 	# Choose a random location on Path2D.
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
@@ -29,11 +24,16 @@ func _on_MobTimer_timeout():
 	add_child(mob)
 	# We connect the mob to the score label to update the score upon squashing a mob.
 	mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
-	mob.initialize(mob_spawn_location.translation, player_position)
+	mob.initialize(mob_spawn_location.translation, player_position, speed2)
 	if Input.is_action_pressed("decrease") :
-		$MobTimer.wait_time += 0.1
+		$MobTimer.wait_time += 0.05
 	if Input.is_action_pressed("increase") and $MobTimer.wait_time>0.2 :
-		$MobTimer.wait_time -= 0.1
+		$MobTimer.wait_time -= 0.05
+	if Input.is_action_pressed("decrease") :
+		speed2-=0.5
+	if Input.is_action_pressed("increase"):
+		speed2+=0.5
+	print("speed2:", speed2)
 
 func _on_Player_hit():
 	$MobTimer.stop()
